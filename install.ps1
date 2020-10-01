aef0dc893f015f7714537d0dcdfa12b96192dde9
Henk Hofs <h.hofs@loginvsi.com>
2020-10-01 14:08:45 +0200

    nuget

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-PackageProvider -Name Nuget -Force -Confirm:$false
+Install-PackageProvider -Name Nuget -Force
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install microsoft-windows-terminal cascadiacodepl -y
