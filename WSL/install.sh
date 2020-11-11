52511c4c5b01e32e744325faa5d63abc489cea09
Henk Hofs <h.hofs@loginvsi.com>
2020-11-11 11:53:04 +0100

    fixed install script for WSL

--- WSL/install.sh
+++ WSL/install.sh
@@ -1,6 +1,12 @@
-wget https://github.com/JanDeDobbeleer/oh-my-posh3/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+#!/bin/bash
+HOME_DIR="$1"
+if [ -z $HOME_DIR ]; then 
+ HOME_DIR="~"
+fi
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+wget -q https://github.com/JanDeDobbeleer/oh-my-posh3/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 chmod +x /usr/local/bin/oh-my-posh
 
-cp -f .zshrc ~
-mkdir -p ~/.poshthemes
-cp -f../.poshthemes/henk.json ~
+cp -f $SCRIPT_DIR/.zshrc $HOME_DIR
+mkdir -p $HOME_DIR/.poshthemes
+cp -f $SCRIPT_DIR/../henk.json $HOME_DIR/.poshthemes
